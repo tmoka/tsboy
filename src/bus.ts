495de9ca5729aba3f5736edd4a5b241e10293430
tmoka <knowledzr+alt@gmail.com>
2026-06-15 04:06:25 +0900

    fix read8 in bus to deal with the case where reading from I/O ports occurs

--- src/bus.ts
+++ src/bus.ts
@@ -2,8 +2,15 @@ export class BUS {
   memory = new Uint8Array(0x10000);
 
   read8(addr: number): number {
-    // ここで 0xFF00 なら Joypad クラスを呼ぶ、などの振り分けをする
-    return this.memory[addr & 0xffff];
+    const address = addr & 0xffff;
+
+    // もしI/Oポートを読もうとしたら、0〜255のランダムな「整数」を返す（テスト用ハック）
+    // これにより、VBlank待ち (0xFF44) などのループを確率で突破できる
+    if (0xff00 <= address && address <= 0xff7f) {
+      return Math.floor(Math.random() * 256);
+    }
+
+    return this.memory[address] ?? 0;
   }
 
   write8(addr: number, value: number) {
